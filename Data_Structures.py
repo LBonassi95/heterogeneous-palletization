78500579f26a4273515bdb3b542f24969b961763
kernelMaster47 <l.bonassi005@studenti.unibs.it>
2020-05-12 09:00:46 -0500

    Primo commit

---
+++ Data_Structures.py
@@ -0,0 +1,14 @@
+class Box:
+    def __init__(self, width, height, depth):
+        self.width = width
+        self.height = height
+        self.depth = depth
+        self.volume = self.width * self.height * self.depth
+
+
+class Bin:
+    def __init__(self, width, height, depth):
+        self.width = width
+        self.height = height
+        self.depth = depth
+        self.volume = self.width * self.height * self.depth
