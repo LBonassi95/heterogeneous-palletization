fe039b28fee033998e6f30ae211789af0545d322
kernelMaster47 <l.bonassi005@studenti.unibs.it>
2020-05-18 16:44:28 +0200

    get_fixed_model aggiunto

--- Hp.py
+++ Hp.py
@@ -8,7 +8,13 @@ def get_random_model(num_of_boxes):
     return ds.PalletizationModel(ds.Bin(100.0, 100.0, 100.0), boxList)
 
 
+def get_fixed_model(num_of_boxes):
+    random.seed(47)
+    boxList = [ds.Box(70, 70, 70) for i in range(num_of_boxes)]
+    return ds.PalletizationModel(ds.Bin(100.0, 100.0, 100.0), boxList)
+
+
 if __name__ == '__main__':
-    model = get_random_model(100)
+    model = get_fixed_model(1723)
     l1 = model.get_l1_w_h()
     print(l1)
