0a0a2fb4742edaa236c4186dff74883650a85d25
castelnovof <52277983+castelnovof@users.noreply.github.com>
2020-05-19 18:09:21 +0200

    Ho aggiunto delle classi (punto2d e punto3d) ed ho implementato il metodo 2d-corners

--- Data_Structures.py
+++ Data_Structures.py
@@ -7,27 +7,90 @@ class Box:
         self.height = height
         self.depth = depth
         self.volume = self.width * self.height * self.depth
-        self.x = -1
-        self.y = -1
-        self.z = -1
+
+        self.position = Point3D(-1, -1, -1)
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
+
+    def get_depth(self):
+        return self.depth
 
     def set_pos(self, x, y, z):
+        self.position = Point3D(x, y, z)
+
+    def get_pos(self):
+        return self.position
+
+    def get_pos_x(self):
+        return self.position[0]
+
+    def get_pos_y(self):
+        return self.position[1]
+
+    def get_pos_z(self):
+        return self.position[2]
+
+    def get_end_x(self):
+        return self.position.get_x() + self.width
+
+    def get_end_y(self):
+        return self.position.get_y() + self.height
+
+    def get_end_z(self):
+        return self.position.get_z() + self.depth
+
+
+class Point2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
+
+    def get_point(self):
+        return self
+
+
+class Point3D:
+    def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
 
-    def get_pos(self):
-        return self.x, self.y, self.z
+    def get_x(self):
+        return self.x
 
-    def get_end_x(self):
-        return self.x + self.width
+    def get_y(self):
+        return self.y
 
-    def get_end_y(self):
-        return self.y + self.height
+    def get_z(self):
+        return self.z
 
-    def get_end_z(self):
-        return self.z + self.depth
+    def set_x(self, x):
+        self.x = x
 
+    def set_y(self, y):
+        self.y = y
+
+    def set_z(self, z):
+        self.z = z
+
+    def get_point(self):
+        return self
 
 class Bin:
     def __init__(self, width, height, depth):
@@ -38,6 +101,15 @@ class Bin:
 
     def get_volume(self):
         return self.volume
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
+
+    def get_depth(self):
+        return self.depth
 
 class PalletizationModel:
     def __init__(self, bin, boxList):
@@ -63,3 +135,47 @@ class PalletizationModel:
             if max_val < max_tmp:
                 max_val = max_tmp
         return j_w_h_d + max_val
+
+    def order_box_set(self, box_set: [Box]):
+        box_set = sorted(box_set, key=lambda box: box.get_end_x(), reverse=True)  # check if it works properly
+        box_set = sorted(box_set, key=lambda box: box.get_end_y(), reverse=True) #check if it works properly
+
+        return box_set
+
+
+    def two_dimensional_corners(self, box_set: [Box], J: [Box], bin: Bin):
+        if box_set is None or len(box_set) == 0:
+            return Point2D(0, 0)
+
+        # must identify the extreme boxes
+        border = 0
+        extreme_boxes = []
+        for box in box_set:
+            if box.get_end_x() > border:
+                extreme_boxes.append(box)
+                border = box.get_end_x()
+        # end of identifying extreme boxes
+
+        # determine the corner points
+        corners = [Point2D(0, extreme_boxes[0].get_end_y())]
+        for index in range(1, len(extreme_boxes)):
+            corners.append(Point2D(corners[index-1].get_end_x(), extreme_boxes[index].get_end_y()))
+        corners.append(Point2D(extreme_boxes[-1].get_end_x(), 0))
+
+        # remove all infeasible corners
+        # a corner is infeasible if in one of the two directions there is no possible box that fits there
+        minimum_w = J[0].get_width()
+        minimum_h = J[0].get_height()
+        for box in J:
+            if box.get_height() > minimum_h:
+                minimum_h = box.get_height()
+            if box.get_width() > minimum_w:
+                minimum_w = box.get_width()
+
+        final_corners = []
+        for corner in corners:
+            if (corner.get_x() + minimum_w) <= bin.get_width() and (corner.get_y() + minimum_h) <= bin.get_height():
+                final_corners.append(corner)
+
+
+
