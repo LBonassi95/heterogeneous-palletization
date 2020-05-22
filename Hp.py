e222712d9afc8cee9a36b6886e0aad7b1d9d4108
kernelMaster47 <l.bonassi005@studenti.unibs.it>
2020-05-22 15:35:41 +0200

    refactoring l1 + test l1

--- Hp.py
+++ Hp.py
@@ -4,8 +4,8 @@ import random
 
 def get_random_model(num_of_boxes):
     random.seed(47)
-    boxList = [ds.Box(float(random.randint(10, 70)), float(random.randint(10, 70)), float(random.randint(10, 70))) for i in range(num_of_boxes)]
-    return ds.PalletizationModel(ds.Bin(100.0, 100.0, 100.0), boxList)
+    boxList = [ds.Box(float(random.randint(1, 10)), float(random.randint(1, 10)), float(random.randint(1, 10))) for i in range(num_of_boxes)]
+    return ds.PalletizationModel(ds.Bin(10, 10, 10), boxList)
 
 
 def get_fixed_model(num_of_boxes):
@@ -16,8 +16,8 @@ def get_fixed_model(num_of_boxes):
 
 if __name__ == '__main__':
     model = get_random_model(100)
-    l1 = model.get_l1_w_h()
+    l1 = model.get_l1_bound()
     print(l1)
-    l2 = model.get_l2_bound()
-    print(l2)
+    # l2 = model.get_l2_bound()
+    # print(l2)
 
--- test_palletizationModel.py
+++ test_palletizationModel.py
@@ -1,18 +1,70 @@
 from unittest import TestCase
+import Data_Structures as ds
 
 
 class TestPalletizationModel(TestCase):
+
     def test_get_l1_bound(self):
-        self.assertEqual(1, 1)
+        boxlist = [ds.Box(6, 6, 6), ds.Box(6, 6, 6), ds.Box(6, 6, 6)]
+        bin = ds.Bin(10, 10, 10)
+        model = ds.PalletizationModel(bin, boxlist)
+        self.assertEqual(3, model.get_l1_bound())
+
 
     def test_get_l1_w_h(self):
-        self.fail()
+        ############################################################
+        boxlist = [ds.Box(6, 6, 7), ds.Box(6, 6, 5), ds.Box(6, 6, 3)]
+        bin = ds.Bin(10, 10, 10)
+        p = 3
+        model = ds.PalletizationModel(bin, boxlist)
+        value_list = [[box.width, box.height, box.depth] for box in boxlist]
+        self.assertEqual(2, model.get_l1_p(p, value_list, bin.width, bin.height, bin.depth))
+        ############################################################
+        boxlist = [ds.Box(6, 6, 7), ds.Box(6, 6, 5), ds.Box(6, 6, 2)]
+        bin = ds.Bin(10, 10, 10)
+        p = 3
+        model = ds.PalletizationModel(bin, boxlist)
+        value_list = [[box.width, box.height, box.depth] for box in boxlist]
+        self.assertEqual(2, model.get_l1_p(p, value_list, bin.width, bin.height, bin.depth))
+        ############################################################
+        boxlist = [ds.Box(6, 6, 5), ds.Box(6, 6, 5), ds.Box(6, 6, 2)]
+        bin = ds.Bin(10, 10, 10)
+        p = 3
+        model = ds.PalletizationModel(bin, boxlist)
+        value_list = [[box.width, box.height, box.depth] for box in boxlist]
+        self.assertEqual(1, model.get_l1_p(p, value_list, bin.width, bin.height, bin.depth))
+        ############################################################
+        boxlist = []
+        bin = ds.Bin(10, 10, 10)
+        p = 3
+        model = ds.PalletizationModel(bin, boxlist)
+        value_list = [[box.width, box.height, box.depth] for box in boxlist]
+        self.assertEqual(0, model.get_l1_p(p, value_list, bin.width, bin.height, bin.depth))
+        ############################################################
+        boxlist = [ds.Box(6, 6, 5), ds.Box(6, 6, 5), ds.Box(6, 6, 2)]
+        bin = ds.Bin(10, 10, 10)
+        p = 5
+        model = ds.PalletizationModel(bin, boxlist)
+        value_list = [[box.width, box.height, box.depth] for box in boxlist]
+        self.assertEqual(1, model.get_l1_p(p, value_list, bin.width, bin.height, bin.depth))
 
     def test_get_l1_w_d(self):
-        self.fail()
+        ############################################################
+        boxlist = [ds.Box(6, 7, 6), ds.Box(6, 5, 6), ds.Box(6, 3, 6)]
+        bin = ds.Bin(10, 10, 10)
+        p = 3
+        model = ds.PalletizationModel(bin, boxlist)
+        value_list = [[box.width, box.depth, box.height] for box in boxlist]
+        self.assertEqual(2, model.get_l1_p(p, value_list, bin.width, bin.depth, bin.height))
 
     def test_get_l1_h_d(self):
-        self.fail()
+        ############################################################
+        boxlist = [ds.Box(7, 6, 6), ds.Box(5, 6, 6), ds.Box(3, 6, 6)]
+        bin = ds.Bin(10, 10, 10)
+        p = 3
+        model = ds.PalletizationModel(bin, boxlist)
+        value_list = [[box.height, box.depth, box.width] for box in boxlist]
+        self.assertEqual(2, model.get_l1_p(p, value_list, bin.height, bin.depth, bin.width))
 
     def test_get_l2_bound(self):
         self.fail()
@@ -26,8 +78,8 @@ class TestPalletizationModel(TestCase):
     def test_get_l2_h_d(self):
         self.fail()
 
-    def test_two_dimensional_corners(self):
-        self.fail()
-
-    def test_three_dimensional_corners(self):
-        self.fail()
+    # def test_two_dimensional_corners(self):
+    #     self.fail()
+    #
+    # def test_three_dimensional_corners(self):
+    #     self.fail()
