76981884dd403ac7ffb7b2bbe2f503185598a63f
kernelMaster47 <l.bonassi005@studenti.unibs.it>
2020-05-22 10:15:08 +0200

    testing

---
+++ test_palletizationModel.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+
+class TestPalletizationModel(TestCase):
+    def test_get_l1_bound(self):
+        self.fail()
+
+    def test_get_l1_w_h(self):
+        self.fail()
+
+    def test_get_l1_w_d(self):
+        self.fail()
+
+    def test_get_l1_h_d(self):
+        self.fail()
+
+    def test_get_l2_bound(self):
+        self.fail()
+
+    def test_get_l2_w_h(self):
+        self.fail()
+
+    def test_get_l2_w_d(self):
+        self.fail()
+
+    def test_get_l2_h_d(self):
+        self.fail()
+
+    def test_two_dimensional_corners(self):
+        self.fail()
+
+    def test_three_dimensional_corners(self):
+        self.fail()
